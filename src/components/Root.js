1d30dea1ca5ab5ee4b14e9e4df945b9c17242de2
Elijah Rhinehardt <elijah.rhinehardt7@gmail.com>
2022-08-21 21:00:44 -0400

    Finished Tier 3

--- src/components/Root.js
+++ src/components/Root.js
@@ -5,16 +5,29 @@ import axios from 'axios';
 // We'll render these sample pets for now. Later, we'll instead fetch the list
 // of pets from the server! We won't need samplePets after that.
 import samplePets from '../petdata';
-const get_pet_data = async () =>  {
-  const {data} = await axios.get('/api/pets');
-  return data;
-}
+
 const Root = () => {
-  const data = get_pet_data();
+  const [data, setData] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState(false)
+
+  // for some reason my on mount isn't working. 'all' can only be seen
+  // after selecting 'cats' or 'dogs'
+  React.useEffect(()=> {
+    fetch('/api/pets')
+    .then(response => {
+      if(response.status === 500) setError(true)
+     return response.json()
+     })
+    .then(data => setData(data))
+    .finally(setLoading(false))
+  }, []);
 
   return (
     <>
       <h1>Adoption Center</h1>
+      <p>{loading ? "Loading":"Finished Loading"}</p>
+      <p>{error ? "Status code 500":"No error!"}</p>
       <PetList pets={data} />
     </>
   )
